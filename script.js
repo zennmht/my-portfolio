7eed03a5379c781f5013235009e4b4bc5a4ef90a
ZennMht12 <zeennxz12@gmail.com>
2023-04-23 23:47:22 +0700

    my portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Student University", "Students", "Qur'an Teacher"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
